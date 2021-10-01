9acd31475e77f2850d662ee46638843aeca5be53
Yon Txpmdroflol <jochong27@gmail.com>
2021-10-01 14:13:17 +0200

    02 - XOXO

--- index.js
+++ index.js
@@ -7,9 +7,35 @@ console.log("01 - Alphabet \n")
 
 function sortLetters(str) {
     var uno = str.split("")
-    var text = uno.sort()
-    text = text.join("")
-    console.log(text)
+    uno = uno.sort().join("")
+    console.log(uno)
 }
 sortLetters("konexio")
 saut2ligne()
+
+//02 - XOXO
+console.log("02 - XOXO \n")
+
+function countEach(str) {
+    var countO = 0
+    var countX = 0
+    for (var i = 0; i < str.length; i++) {
+        if (str[i] == "o") {
+            countO++
+        } else {
+            countX++
+        }
+    }
+    console.log(countO)
+    console.log(countX)
+    if (countO == countX) {
+        console.log(true)
+    } else {
+        console.log(false)
+    }
+}
+countEach("xxxoooox")
+countEach("xox")
+saut2ligne()
+
+//03 - Palindrome
